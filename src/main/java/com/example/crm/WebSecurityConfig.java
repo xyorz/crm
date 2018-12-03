6effdef4db51a5429b3af31de0072af5e3acf06a
hyq <544608734@qq.com>
2018-12-03 22:31:29 +0800

    update login

---
+++ src/main/java/com/example/crm/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.example.crm;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Configuration
+public class WebSecurityConfig extends WebMvcConfigurerAdapter {
+    public final static String SESSION_KEY="employeenum";
+
+//    @Bean
+//    public SecurityInterceptor getSecurityInterceptor(){
+//        return new SecurityInterceptor();
+//    }
+
+    public void  addInterceptors(InterceptorRegistry registry){
+//        InterceptorRegistration addInterceptor = registry.addInterceptor(getSecurityInterceptor());
+
+       // addInterceptor.excludePathPatterns("/error");
+        //addInterceptor.excludePathPatterns("/login**");
+        //addInterceptor.addPathPatterns("/**");
+        registry.addInterceptor(new SecurityInterceptor()).addPathPatterns("/**").excludePathPatterns("/login**").excludePathPatterns("/error");
+    }
+
+    private class SecurityInterceptor extends HandlerInterceptorAdapter {
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+            HttpSession session = request.getSession();
+
+//            判断是否已有该用户登录的session
+            if(session.getAttribute(SESSION_KEY) != null){
+                return true;
+            }
+
+//            跳转到登录页
+            String url = "/login";
+            response.sendRedirect(url);
+            return false;
+        }
+    }
+}
+
+
+
+
--- src/main/java/com/example/crm/controller/CRMController.java
+++ src/main/java/com/example/crm/controller/CRMController.java
@@ -1,25 +1,47 @@
 package com.example.crm.controller;
 
-import com.example.crm.entity.Customer;
+import com.example.crm.WebSecurityConfig;
+import com.example.crm.entity.Employee;
 import com.example.crm.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Controller
-@RequestMapping(path = "")
+//@RequestMapping(path = "")
 public class CRMController {
 
-    @RequestMapping(path = "")
+//    @RequestMapping(path = "")
+//    public String index(){
+//        return "index";
+//    }
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginVerify(String username, String password, HttpSession session){
+
+        Optional<Employee> optionalEmployee = Optional.of(employeeRepository.findByIdAndPassword(Integer.valueOf(username),password));
+        if (optionalEmployee.isPresent()){
+            session.setAttribute(WebSecurityConfig.SESSION_KEY, username);
+            return "index";
+        }
+        return "redirect:/login";
     }
 }
 
