28ec3f06dc754477632c14113f1c999476cb7391
hyq <544608734@qq.com>
2018-12-04 20:08:08 +0800

    登陆修改

--- src/main/java/com/example/crm/controller/CRMController.java
+++ src/main/java/com/example/crm/controller/CRMController.java
@@ -5,21 +5,13 @@ import com.example.crm.entity.Employee;
 import com.example.crm.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
-import java.util.Optional;
 
 @Controller
-//@RequestMapping(path = "")
 public class CRMController {
-
-//    @RequestMapping(path = "")
-//    public String index(){
-//        return "index";
-//    }
 
     @Autowired
     EmployeeRepository employeeRepository;
@@ -34,12 +26,6 @@ public class CRMController {
         return "login";
     }
 
-//    @GetMapping("/index")
-//    public String index1()
-//    {
-//        return "index";
-//    }
-
     @PostMapping("/login")
     public ModelAndView loginVerify(String username, String password, HttpSession session){
         ModelAndView mav = new ModelAndView();
@@ -49,17 +35,24 @@ public class CRMController {
             Employee employee = employeeRepository.findByIdAndPassword(id,password);
             if (employee != null){
                 session.setAttribute(WebSecurityConfig.SESSION_KEY, username);
-                mav.setViewName("/");
+                mav.setViewName("index");
             }
+            else {
+                mav.setViewName("login");
+                mav.addObject("errorInfo", "用户名或密码错误");
+            }
+        }
+        catch (NumberFormatException nfe){
             mav.setViewName("login");
-            mav.addObject("errorInfo","用户名或密码错误");
-
+            mav.addObject("errorInfo","用户名格式错误");
         }
         catch (Exception e){
-
+            mav.setViewName("login");
+            mav.addObject("errorInfo","错误");
         }
         return mav;
-
     }
+
 }
+
 
