3f86070dc78b05480dfa6d48c1a5dc0487b39bdb
hyq <544608734@qq.com>
2018-12-18 16:03:35 +0800

    update login

--- src/main/resources/static/assets/scripts/login.js
+++ src/main/resources/static/assets/scripts/login.js
@@ -1,67 +1,68 @@
 var Login = function () {
 
-	var handleLogin = function() {
-		$('.login-form').validate({
-	            errorElement: 'span', //default input error message container
-	            errorClass: 'help-block', // default input error message class
-	            focusInvalid: false, // do not focus the last invalid input
-	            rules: {
-	                username: {
-	                    required: true
-	                },
-	                password: {
-	                    required: true
-	                },
-	                remember: {
-	                    required: false
-	                }
-	            },
+    var handleLogin = function() {
+        $('.login-form').validate({
+            errorElement: 'span', //default input error message container
+            errorClass: 'help-block', // default input error message class
+            focusInvalid: false, // do not focus the last invalid input
+            rules: {
+                username: {
+                    required: true,
+                    digits:true
+                },
+                password: {
+                    required: true
+                },
+                remember: {
+                    required: false
+                }
+            },
+            messages: {
+                username: {
+                    required: "账号不能为空.",
+                    digits:"账号必须为数字"
+                },
+                password: {
+                    required: "密码不能为空"
+                }
+            },
 
-	            messages: {
-	                username: {
-	                    required: "用户名不能为空."
-	                },
-	                password: {
-	                    required: "密码不能为空"
-	                }
-	            },
+            invalidHandler: function (event, validator) { //display error alert on form submit
+                $('.alert-danger1', $('.login-form')).show();
+                $('.alert-danger2', $('.login-form')).hide();
+            },
 
-	            invalidHandler: function (event, validator) { //display error alert on form submit   
-	                $('.alert-danger', $('.login-form')).show();
-	            },
+            highlight: function (element) { // hightlight error inputs
+                $(element)
+                    .closest('.form-group').addClass('has-error'); // set error class to the control group
+            },
 
-	            highlight: function (element) { // hightlight error inputs
-	                $(element)
-	                    .closest('.form-group').addClass('has-error'); // set error class to the control group
-	            },
+            success: function (label) {
+                label.closest('.form-group').removeClass('has-error');
+                label.remove();
+            },
 
-	            success: function (label) {
-	                label.closest('.form-group').removeClass('has-error');
-	                label.remove();
-	            },
+            errorPlacement: function (error, element) {
+                error.insertAfter(element.closest('.input-icon'));
+            },
 
-	            errorPlacement: function (error, element) {
-	                error.insertAfter(element.closest('.input-icon'));
-	            },
+            submitHandler: function (form) {
+                form.submit();
+            }
+        });
 
-	            submitHandler: function (form) {
-	                form.submit();
-	            }
-	        });
-
-	        $('.login-form input').keypress(function (e) {
-	            if (e.which == 13) {
-	                if ($('.login-form').validate().form()) {
-	                    $('.login-form').submit();
-	                }
-	                return false;
-	            }
-	        });
-	}
+        $('.login-form input').keypress(function (e) {
+            if (e.which === 13) {
+                if ($('.login-form').validate().form()) {
+                    $('.login-form').submit();
+                }
+                return false;
+            }
+        });
+    }
     return {
         //main function to initiate the module
         init: function () {
-        	
             handleLogin();
         }
 
